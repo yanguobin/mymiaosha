64120f193d1992ee3e8d2868bbfb79723f29b28a
yanguobin <yan_guo_bin@163.com>
2019-11-12 11:43:55 +0800

    Result类中添加包含code、msg参数的默认构造方法，为访问失败情况下做基础封装准备

--- mymiaosha1/src/main/java/com/example/mymiaosha1/result/Result.java
+++ mymiaosha1/src/main/java/com/example/mymiaosha1/result/Result.java
@@ -5,6 +5,11 @@ public class Result<T> {
     private String msg;
     private T data;
 
+    public Result(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
     public Result(Integer code, String msg, T data) {
         this.code = code;
         this.msg = msg;
