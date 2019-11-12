61c981e4ec819931fd19ca960bbc3f71f74adcb3
yanguobin <yan_guo_bin@163.com>
2019-11-12 10:53:07 +0800

    成功新建mymiaosha1模块为Spring Boot项目

---
+++ mymiaosha1/src/test/java/com/example/mymiaosha1/Mymiaosha1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymiaosha1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mymiaosha1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
