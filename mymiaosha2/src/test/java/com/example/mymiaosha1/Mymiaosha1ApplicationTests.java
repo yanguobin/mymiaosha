6e12977e4dddc579d286b5e8f21172af28382d04
yanguobin <yan_guo_bin@163.com>
2019-11-12 13:41:58 +0800

    复制mymiaosha1模块为mymiaosha2，之后在mymiaosha2模块上开发

---
+++ mymiaosha2/src/test/java/com/example/mymiaosha1/Mymiaosha1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mymiaosha1;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mymiaosha1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
