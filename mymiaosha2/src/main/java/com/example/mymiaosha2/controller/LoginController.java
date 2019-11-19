af7eafbc00a73f55efab813774b751937ab8e20e
yanguobin <yan_guo_bin@163.com>
2019-11-19 11:30:03 +0800

    1、login.html 添加用户登录页面，使用Thymeleaf模版构建。先引入各个脚本文件，注意Thymeleaf引入文件的方式为@{}，如<script type=“text/javascript” th:src=“@{/js/jquery.min.js}”></script>，其中，/js/jquery.min.js中开头的/代表static路径。页面基于bootstrap构建，注意到input标签中的required=“true” minlength=“11” maxlength=“11”是由jquery-validation提供的。form标签中的style=“width:50%; margin:0 auto”>表示表单宽度为屏幕的50%，位置为居中。 1.1、登录按钮的功能由本页中的js脚本实现，js中使用ajax实现异步调用。注意data中的password为第一次加密后的密码，所以需要提前写好。sucess回调中先在控制台中输出测试。 2、LoginVo.java 新建类，封装mobile和password。 3、LoginController.java 修改doLogin方法，入参LoginVo类型参数，方法体中先打印日志进行测试。

--- mymiaosha2/src/main/java/com/example/mymiaosha2/controller/LoginController.java
+++ mymiaosha2/src/main/java/com/example/mymiaosha2/controller/LoginController.java
@@ -2,6 +2,9 @@ package com.example.mymiaosha2.controller;
 
 import com.example.mymiaosha2.redis.MyRedisUtil;
 import com.example.mymiaosha2.result.Result;
+import com.example.mymiaosha2.vo.LoginVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    private static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
     MyRedisUtil myRedisUtil;
@@ -21,7 +26,8 @@ public class LoginController {
 
     @RequestMapping("/do_login")
     @ResponseBody
-    public Result<Boolean> doLogin(){
+    public Result<Boolean> doLogin(LoginVo loginVo){
+        logger.info(loginVo.toString());
         return null;
     }
 }
---
+++ mymiaosha2/src/main/java/com/example/mymiaosha2/vo/LoginVo.java
@@ -0,0 +1,31 @@
+package com.example.mymiaosha2.vo;
+
+public class LoginVo {
+
+    private String mobile;
+    private String password;
+
+    @Override
+    public String toString() {
+        return "LoginVo{" +
+                "mobile='" + mobile + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
