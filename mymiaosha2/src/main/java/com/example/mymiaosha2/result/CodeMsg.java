90c7f94070544b0d57ec51483b119be4f3c4c470
yanguobin <yan_guo_bin@163.com>
2019-11-22 17:13:00 +0800

    标题：异常处理 1、GlobalExceptionHandler.java 新建类，添加exceptionHandler方法，该方法需标注@ExceptionHandler(value = Exception.class) 表示要拦截所有的异常，方法体内先拦截刚才的BindException，步骤依次为异常类型强转，获取所有错误信息，拿到第一个错误，拿到具体错误信息，最后拼接完后返回。拼接调用接下来的CodeMsg.java类中的fillArgs方法。 2、CodeMsg.java 添加绑定异常静态常量BIND_ERROR，注意他的msg参数处留一个位置。添加fillArgs方法，使用String.format()做拼接，供第一步拼接时调用。 写在最后：这样，浏览器中的异常信息，经过上述代码就可以捕获并处理，最终友好的显示在浏览器页面。

--- mymiaosha2/src/main/java/com/example/mymiaosha2/result/CodeMsg.java
+++ mymiaosha2/src/main/java/com/example/mymiaosha2/result/CodeMsg.java
@@ -6,11 +6,20 @@ public class CodeMsg {
 
     public static final CodeMsg SUCCESS = new CodeMsg(0, "success");
     public static final CodeMsg SERVER_ERROR = new CodeMsg(500100, "服务器异常");
+    public static final CodeMsg BIND_ERROR = new CodeMsg(500101, "参数校验异常：%s");
+
+    //登录模块
     public static final CodeMsg PASSWORD_EMPTY = new CodeMsg(500211, "登录密码不能为空");
     public static final CodeMsg MOBILE_EMPTY = new CodeMsg(500212, "手机号不能为空");
     public static final CodeMsg MOBILE_ERROR = new CodeMsg(500213, "手机号格式错误");
     public static final CodeMsg MOBILE_NOT_EXIST = new CodeMsg(500214, "手机号不存在");
     public static final CodeMsg PASSWORD_ERROR = new CodeMsg(500215, "密码错误");
+
+    public CodeMsg fillArgs(Object... args){
+        int code = this.code;
+        String message = String.format(this.msg, args);
+        return new CodeMsg(code, message);
+    }
 
     private CodeMsg(Integer code, String msg) {
         this.code = code;
