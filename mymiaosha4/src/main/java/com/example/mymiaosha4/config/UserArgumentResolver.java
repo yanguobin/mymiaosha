e5a2b2d8ce141f0d6ea51f623d0cf4263290c26e
yanguobin <yan_guo_bin@163.com>
2019-11-27 15:33:06 +0800

    修复bug

--- mymiaosha4/src/main/java/com/example/mymiaosha4/config/UserArgumentResolver.java
+++ mymiaosha4/src/main/java/com/example/mymiaosha4/config/UserArgumentResolver.java
@@ -44,6 +44,9 @@ public class UserArgumentResolver implements HandlerMethodArgumentResolver {
 
     private String getCookieValue(HttpServletRequest request, String cookieNameToken) {
         Cookie[] cookies = request.getCookies();        //获取所有的cookie
+        if (cookies == null || cookies.length <= 0){
+            return null;
+        }
         for (Cookie cookie : cookies){
             if (cookie.getName().equals(cookieNameToken)){
                 return cookie.getValue();
