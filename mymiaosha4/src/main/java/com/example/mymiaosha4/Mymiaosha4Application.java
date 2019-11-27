d6feb2b7d622052725b77d2f2e858459b42d4192
yanguobin <yan_guo_bin@163.com>
2019-11-27 17:41:59 +0800

    打war包需要做的准备工作

--- mymiaosha4/src/main/java/com/example/mymiaosha4/Mymiaosha4Application.java
+++ mymiaosha4/src/main/java/com/example/mymiaosha4/Mymiaosha4Application.java
@@ -2,12 +2,18 @@ package com.example.mymiaosha4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Mymiaosha4Application {
+public class Mymiaosha4Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Mymiaosha4Application.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Mymiaosha4Application.class);
+    }
 }
