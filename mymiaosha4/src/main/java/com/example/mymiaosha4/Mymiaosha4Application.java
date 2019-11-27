edf0b79d1ee81c7f514884dabd43c99401b423a6
yanguobin <yan_guo_bin@163.com>
2019-11-27 17:43:22 +0800

    改回为打Jar包的方式

--- mymiaosha4/src/main/java/com/example/mymiaosha4/Mymiaosha4Application.java
+++ mymiaosha4/src/main/java/com/example/mymiaosha4/Mymiaosha4Application.java
@@ -2,18 +2,12 @@ package com.example.mymiaosha4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Mymiaosha4Application extends SpringBootServletInitializer {
+public class Mymiaosha4Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Mymiaosha4Application.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(Mymiaosha4Application.class);
-    }
 }
