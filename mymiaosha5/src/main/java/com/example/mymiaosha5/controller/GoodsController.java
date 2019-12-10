b57afa1d2832c75a3094a168eb28038211667720
yanguobin <yan_guo_bin@163.com>
2019-12-10 20:20:14 +0800

    对商品列表功能做页面缓存

--- mymiaosha5/src/main/java/com/example/mymiaosha5/controller/GoodsController.java
+++ mymiaosha5/src/main/java/com/example/mymiaosha5/controller/GoodsController.java
@@ -1,15 +1,24 @@
 package com.example.mymiaosha5.controller;
 
 import com.example.mymiaosha5.domain.MiaoshaUser;
+import com.example.mymiaosha5.redis.GoodsKey;
+import com.example.mymiaosha5.redis.MyRedisUtil;
 import com.example.mymiaosha5.service.GoodsService;
 import com.example.mymiaosha5.service.MiaoshaUserService;
 import com.example.mymiaosha5.vo.GoodsVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.spring4.context.SpringWebContext;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @Controller
@@ -22,13 +31,35 @@ public class GoodsController {
     @Autowired
     GoodsService goodsService;
 
-    @RequestMapping("/to_list")
-    public String list(Model model, MiaoshaUser user) {
+    @Autowired
+    MyRedisUtil myRedisUtil;
+
+    @Autowired
+    ThymeleafViewResolver thymeleafViewResolver;
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @RequestMapping(value = "/to_list", produces = "text/html")
+    @ResponseBody
+    public String list(HttpServletRequest request, HttpServletResponse response, Model model, MiaoshaUser user) {
         model.addAttribute("user", user);
         //查询商品列表，包括商品和秒杀商品
         List<GoodsVo> goodsList = goodsService.listGoodsVo();
         model.addAttribute("goodsList", goodsList);     //放到Model中，供前端展示使用。
-        return "goods_list";
+//        return "goods_list";
+        //取缓存
+        String html = myRedisUtil.get(GoodsKey.getGoodsList, "", String.class);
+        if (!StringUtils.isEmpty(html)){
+            return html;
+        }
+        //手动渲染
+        SpringWebContext ctx = new SpringWebContext(request, response, request.getServletContext(), request.getLocale(), model.asMap(), applicationContext);
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_list", ctx);
+        if (!StringUtils.isEmpty(html)){
+            myRedisUtil.set(GoodsKey.getGoodsList, "", html);
+        }
+        return html;
     }
 
     @RequestMapping("/to_detail/{goodsId}")
