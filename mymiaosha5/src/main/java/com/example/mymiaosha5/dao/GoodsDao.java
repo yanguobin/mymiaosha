ffb26f25748e0a4142b8297c6d778ca6f91993e6
yanguobin <yan_guo_bin@163.com>
2019-12-23 18:34:51 +0800

    解决卖超，防止商品数量减成负数

--- mymiaosha5/src/main/java/com/example/mymiaosha5/dao/GoodsDao.java
+++ mymiaosha5/src/main/java/com/example/mymiaosha5/dao/GoodsDao.java
@@ -41,6 +41,6 @@ public interface GoodsDao {
     })
     public GoodsVo getGoodsVoByGoodsId(@Param("goodsId") long goodsId);
 
-    @Update("update miaosha_goods set stock_count = stock_count - 1 where goods_id = #{goodsId}")
+    @Update("update miaosha_goods set stock_count = stock_count - 1 where goods_id = #{goodsId} and stock_count>0")
     public int reduceStock(MiaoshaGoods miaoshaGoods);
 }
